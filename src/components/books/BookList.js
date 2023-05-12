22c8288cce7b93d0ee60f2bed0fc059e99d593da
Gabriel Dela Pena <delapena.gabriel12@gmail.com>
2023-05-12 19:59:00 +0200

    Added run server method

--- src/components/books/BookList.js
+++ src/components/books/BookList.js
@@ -7,6 +7,7 @@ import BookItem from "./BookItem.";
 function BookList() {
   // const booksCtx = useContext(BookContext);
   const [books, setBooks] = useState([]);
+  const [serverStatus, setServerStatus] = useState();
 
   useEffect(() => {
     fetch("https://library-server-node.onrender.com/books")
@@ -20,9 +21,25 @@ function BookList() {
       });
   }, []);
 
+  const handleRunServer = () => {
+    fetch("https://library-server-node.onrender.com/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setServerStatus(data)
+      });
+  }
+
   return (
     <section className={classes.listContainer}>
-      <p className={classes.title}>This app is designed for Node.js development.</p>
+      <div className={classes.titleStyling}>
+        <p className={classes.title}>This app is designed for Node.js development.</p>
+        <p>Please click on run server to run the backend server.</p>
+        <button onClick={handleRunServer} className={classes.btnStyle}>Run Server</button>
+        <p>{serverStatus ? serverStatus : ""}</p>
+      </div>
+
       <ul className={classes.bookList}>
         {books?.map((book, index) => {
           return <BookItem key={index} book={book} />;
